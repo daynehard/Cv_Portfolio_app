f9e40611699a284768e63afd7f73735cdb671eaf
Ian Deinhard <iandeinhard254@gmail.com>
2023-03-27 09:23:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cvportfolio/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cvportfolio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn_introduce = findViewById<Button>(R.id.btnintroduce)
+    }
+}
